43ad51b198ed6e9597d9671b0c9e24673f571f00
Matteo Enna <info@matteoenna.it>
2019-06-11 14:34:29 +0200

    Update RoarController.php

--- web/modules/dino_roar/src/Controller/RoarController.php
+++ web/modules/dino_roar/src/Controller/RoarController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 class RoarController extends ControllerBase
 {
     /**
-     * @var RoarGenerator
+     * @var RoarGenerator 
      */
     private $roarGenerator;
     /**
